00da50d45c715c8ed4688332eb3926d1fb6e50c2
AndersUOlsson <olsson.anders@protonmail.com>
2017-10-21 16:49:28 +0200

    Structure
    Enemy object class created

---
+++ src/objects/EnemyObject.java
@@ -0,0 +1,12 @@
+package objects;
+
+import javafx.scene.Node;
+
+public class EnemyObject extends Object {
+
+	public EnemyObject(Node view) {
+		super(view);
+		// TODO Auto-generated constructor stub
+	}
+
+}
